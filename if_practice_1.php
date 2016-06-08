95a537261cddacf439480881e748b16e3187a15a
otake <otake.hiroyo@gmail.com>
2016-06-08 11:30:17 +0000

    if文を１つにまとめる

--- if_practice_1.php
+++ if_practice_1.php
@@ -1,6 +1,11 @@
 <?php
 // 24時間単位の現在時
 $hour = date('G');
+if ($hour>=6 && $hour<=17){
+  $daynight = 'day';
+   } else {
+  $daynight = 'night';
+ }
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,23 +14,7 @@ $hour = date('G');
   <title></title>
   <link href="style.css" rel="stylesheet" type="text/css">
 </head>
-<body
-  <?php 
-  if ($hour>=6 && $hour<=17){
-    echo ' class="day"';
-     } else {
-    echo ' class="night"';
-   }
-  ?>
->
-<h1 
-  <?php 
-  if ($hour>=6 && $hour<=17){
-    echo ' class="day"';
-     } else {
-    echo ' class="night"';
-   }
-  ?>
->昼と夜でページの見た目が変わります。</h1>
+<body class="<?php echo $daynight ?>">
+<h1 class="<?php echo $daynight ?>">昼と夜でページの見た目が変わります。</h1>
 </body>
 </html>
